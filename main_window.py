480e538e604cca380d694b0b69ad09ca252229cb
Szelmat <szelessm@gmail.com>
2020-12-03 16:20:46 +0100

    restructure code

---
+++ main_window.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import (
+    QWidget, 
+    QDesktopWidget, 
+    QApplication, 
+    QToolBar,
+    QGridLayout,
+    QPushButton,
+)
+
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.resize(1200, 800)
+        self.build_layout()
+        self.center()
+        self.setWindowTitle('BreakApp')
+        self.show()
+
+    def build_layout(self):
+        '''Add elements to the window to the correct place'''
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
+        self.layout.addWidget(QPushButton('1'),0,0) # This is just a test
+
+    def center(self):
+        '''Center the window'''
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mw = MainWindow()
+    sys.exit(app.exec_())
